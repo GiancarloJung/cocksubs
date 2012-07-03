8b7eb98382deeea9b31c76a33ceceacc8a3f18a9
Giancarlo Jung <g8.jung@gmail.com>
2012-07-03 07:58:00 -0300

    git init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cocksubs::Application.config.secret_token = '2fd9f0a473b9a6acf98afe25e44f737c54759aeb3be23744fdc2aff7e7d97d8f526a7ff3123f68291be81e63a9fa4eb02df2980807213050b611cbc7166ea9a5'
